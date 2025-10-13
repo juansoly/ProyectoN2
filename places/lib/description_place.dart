f86a21b199e48bd74a98395d131d62ca8f0efec2
juansoly <solizsoriajuandiego002@gmail.com>
2025-10-13 11:25:17 -0400

    subido el 12/10/25

---
+++ places/lib/description_place.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+class DescriptionPlace extends StatelessWidget{
+  //VARIABLES
+  String textoTitulo;
+  int cantidadEstrellas;
+  String textoDescripcion;
+
+  DescriptionPlace(this.textoTitulo,this.cantidadEstrellas, this.textoDescripcion);
+
+  @override
+  Widget build(BuildContext context) {
+    final titulo = Container(
+      margin: EdgeInsets.only(
+        right: 20
+      ),
+      child: Text(
+        textoTitulo,
+        style: TextStyle(
+          fontFamily: "Lato",
+          fontSize: 38,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+    );
+    final estrella = Container(
+      margin: EdgeInsets.only(
+        right: 5
+      ),
+      child: Icon(
+        Icons.star,
+        color:Colors.amber,
+      ),
+    );
+    final estrellaBorde = Container(
+      margin: EdgeInsets.only(
+        right: 5
+      ),
+      child: Icon(
+        Icons.star_border,
+        color: Colors.black54,
+      ),
+    );
+    //Fila estrella
+    List <Container> estrellas = [];
+    for(int i=0; i<5; i++){
+      if(i < cantidadEstrellas) {
+        estrellas.add(estrella);
+      }else{
+        estrellas.add(estrellaBorde);
+      }
+      }
+
+    final filaEstrellas = Row(
+      children:estrellas,
+
+    );
+    final filaTitulo=Row(
+      children: <Widget>[
+        titulo,
+        filaEstrellas
+      ],
+    );
+    final descripcion= Container(
+      margin: EdgeInsets.only(
+        top: 10
+      ),
+      child: Text(
+        textoDescripcion,
+        style: TextStyle(
+          fontFamily: "Lato",
+          color: Colors.black54
+        ),
+
+      ),
+
+    );
+    final descriptionPlace = Column(
+      children: <Widget>[
+        filaTitulo,
+        descripcion
+      ],
+    );
+    return descriptionPlace;
+  }
+
+}
